9f1aa3e84d2fa0ba4df9a9830de720d566140072
Jorge Cardoso <129989570+JORGECARDOSODEV@users.noreply.github.com>
2024-02-07 22:23:13 -0300

    Arquivo index com resolução do desafio

---
+++ index.js
@@ -0,0 +1,34 @@
+let nomeHeroi = "Thor"
+let nivelXp = 12000
+
+if((nivelXp > 0) && (nivelXp < 1000)){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Ferro");
+  }
+
+if((nivelXp >= 1001) && (nivelXp < 2000)){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Bronze");
+  }
+
+if((nivelXp >= 2001) && (nivelXp <= 5000)){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Prata");
+  }
+
+if((nivelXp >= 5001) && (nivelXp <= 7000)){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Ouro");
+  }
+
+if((nivelXp >= 7001) && (nivelXp <= 8000)){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Platina");
+  }
+
+if((nivelXp >= 8001) && (nivelXp <= 9000)){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Ascendente");
+  }
+
+if((nivelXp >= 9001) && (nivelXp <= 10000)){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Imortal");
+  }
+  
+if((nivelXp >= 10001)){
+    console.log("O Herói de nome " + nomeHeroi + " está no nível Radiante");
+  }
